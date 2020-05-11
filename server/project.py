5634404940da4a8363ab65f64b0aeaa6e06342d3
Ebram Michel <ebrammichel16@gmail.com>
2020-05-11 22:17:12 +0200

    Finishing Extract Function

--- server/project.py
+++ server/project.py
@@ -26,3 +26,28 @@ def extract_text(image,no_of_ids):
         cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    
+        # loop over the contours
+    displayCnt = None
+    i=0
+    n_id=no_of_ids
+    for c in cnts:        
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:
+            displayCnt = approx
+            output = four_point_transform(image, displayCnt.reshape(4, 2))
+            if(output.shape[1]>output.shape[0]):
+                dim = (300, 180)
+                output = cv2.resize(output, dim) 
+                crop_img = output[115:145, 90:160]
+            else :
+                dim = (180, 300)
+                output = cv2.resize(output, dim) 
+                crop_img = output[195:215, 40:100]   
+            ids.append(pytesseract.image_to_string(crop_img,lang='eng'))
+            i=i+1  
+            if i==n_id:
+                break         
+    with open('list.txt', 'w') as filehandle:
+         filehandle.writelines("%s\n" % i for i in ids) 
