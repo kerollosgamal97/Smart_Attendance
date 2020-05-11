d254802c03d96b04dedb22c327a37ebfbde6582c
Kerollos Gamal <kerollosgamal@gmail.com>
2020-05-11 22:11:27 +0200

    extract and sort the contours

--- server/project.py
+++ server/project.py
@@ -13,3 +13,16 @@ import socket
 import os
 import _thread
 import math
+
+def extract_text(image,no_of_ids):
+    ids=[]
+    image=cv2.resize(image,(850,850))
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5,5), 0)
+    edged = cv2.Canny(blurred, 30, 110, 30)
+    # find contours in the edge map, then sort them by their
+    # size in descending order
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
